5a456c3e0ce27236db86ef470bb99c5cb1ffa7b2
George <georgecharleschang@gmail.com>
2019-03-11 18:27:28 -0700

    Generate new components

--- angular-recipe-project/src/app/app.module.ts
+++ angular-recipe-project/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    IngredientComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule

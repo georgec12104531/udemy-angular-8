886123291ef6b46da4e263667c41c6fb726f3052
George <georgecharleschang@gmail.com>
2019-03-12 10:34:09 -0700

    Add recipe model

--- angular-recipe-project/src/app/recipes/recipes-list/recipes-list.component.ts
+++ angular-recipe-project/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import Recipe from '../recipe.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipie', 'this is a test', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg'),
+    new Recipe('A test recipie', 'this is a test', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg'),
+    new Recipe('A test recipie', 'this is a test', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg')
+  ];
 
   constructor() { }
 

dea7e12ff222622cb56d9b2e6aa14a14b4736b58
George <georgecharleschang@gmail.com>
2019-03-17 21:12:55 -0700

    Split app into two components, binding to custom events

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -8,4 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   serverElements = [{type: 'server', name: 'testdfdf_name', content: 'test_condfdtent'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+
+
 }

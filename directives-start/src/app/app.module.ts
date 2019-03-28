bacee7d873872474d7c4d0da52c7889ef881a6b8
George <georgecharleschang@gmail.com>
2019-03-28 07:51:32 -0700

    Add better directive using renderer2

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,

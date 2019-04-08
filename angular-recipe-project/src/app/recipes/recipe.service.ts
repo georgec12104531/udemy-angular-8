c7354f2c104ffcd033cfcf9544a9ca1bdf3d939a
George <georgecharleschang@gmail.com>
2019-04-08 08:25:01 -0700

    Add some recipes

--- angular-recipe-project/src/app/recipes/recipe.service.ts
+++ angular-recipe-project/src/app/recipes/recipe.service.ts
@@ -1,14 +1,36 @@
 import { EventEmitter } from '@angular/core';
 import Recipe from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipie', 'this is a test', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg'),
-    new Recipe('A test recipie', 'this is a test', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg'),
-    new Recipe('A test recipie', 'this is a test', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg')
+    new Recipe(
+      'A Fat Buger',
+      'This burger is wayyyyy PHAT',
+      'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg',
+      [
+        new Ingredient('Buns', 10),
+        new Ingredient('Meat', 2),
+        new Ingredient('fries', 25)
+      ]),
+    new Recipe(
+      'Hottie Doggystyle',
+      'mmmhmmmm',
+      'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Sausage', 1)
+      ]),
+    new Recipe(
+      'Coffee',
+      'A cup of Joe',
+      'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32-1100x1650.jpg',
+      [
+        new Ingredient('Scoops of Coffee Beans', 3)
+      ])
   ];
 
   getRecipes() {

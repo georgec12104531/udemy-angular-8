56b2ec8b473060efc11315357595aed1217c86c2
George Chang <george.changfrequence.com@Georges-MacBook-Pro.local>
2019-04-28 15:04:47 -0700

    Add editing routes

---
+++ angular-recipe-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-recipe-edit',
+    templateUrl: './recipe-edit.component.html',
+    styleUrls: ['./recipe-edit.component.css']
+  })
+
+export class RecipeEditComponent {
+
+}

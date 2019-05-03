423bb6f3af574f817183c8d2b8f868662be60b2e
George <georgecharleschang@gmail.com>
2019-05-03 09:10:44 -0700

    Halfway complete with using subjects to pass and listen to data

--- observables-start/src/app/app.component.ts
+++ observables-start/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { UsersService } from './user/users.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  user1Activated = false;
+  user2Activated = false;
+
+  constructor(private usersService:UsersService) {}
+
+  ngOnInit() {
+    this.usersService.userActivated.subscribe()
+  }
 }

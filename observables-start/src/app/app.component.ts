b4ecb0df9eb58fa8d5b04d5dbffd3350909f7a81
George <georgecharleschang@gmail.com>
2019-05-05 21:33:49 -0700

    Fix error

--- observables-start/src/app/app.component.ts
+++ observables-start/src/app/app.component.ts
@@ -10,9 +10,17 @@ export class AppComponent implements OnInit {
   user1Activated = false;
   user2Activated = false;
 
-  constructor(private usersService:UsersService) {}
+  constructor(private usersService: UsersService) {}
 
   ngOnInit() {
-    this.usersService.userActivated.subscribe()
+    this.usersService.userActivated.subscribe(
+      (id: number) => {
+        if (id === 1) {
+          this.user1Activated = true;
+        } else if (id === 2) {
+          this.user2Activated = true;
+        }
+      }
+    );
   }
 }

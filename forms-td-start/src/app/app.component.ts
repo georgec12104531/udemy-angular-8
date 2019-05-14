7d251056c74f1b6d5daaa32e3e2884becbc36a35
George <georgecharleschang@gmail.com>
2019-05-14 09:19:29 -0700

    Set and patch form values q

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -10,9 +10,32 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'teacher';
   answer = '';
+  genders = ['male', 'female'];
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -20,6 +43,10 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.username;
+    this.user.username = this.signupForm.value.userData.email;
+    this.user.username = this.signupForm.value.username;
+    this.user.username = this.signupForm.value.username;
+    this.user.username = this.signupForm.value.username;
   }
 }

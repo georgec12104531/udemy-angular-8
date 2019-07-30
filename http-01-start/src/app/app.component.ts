3f03e8a184ef6065e4a6ab24df3ec6564721dc79
George <georgecharleschang@gmail.com>
2019-07-29 22:36:07 -0700

    Add loader

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model'
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -38,6 +39,7 @@ export class AppComponent implements OnInit {
   }
 
   fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{[key: string]: Post}>('https://http-practice-01.firebaseio.com/post.json')
       .pipe(map((response) => {
@@ -51,6 +53,7 @@ export class AppComponent implements OnInit {
       })
     )
       .subscribe((posts) => {
+        this.isFetching = false;
         this.loadedPosts = posts;
     });
   }
